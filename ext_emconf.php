ac17875df03f17f4f196490870078b7fd558972e
Tuurlijk <Tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-02-23 16:15:13 +0000

    Rename modfunc1 to infomodule
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nc_staticfilecache/trunk@8358 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "nc_staticfilecache"
 #
-# Auto generated 28-06-2007 16:38
+# Auto generated 23-02-2008 17:11
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:36:"class.tx_ncstaticfilecache.debug.php";s:4:"3f9a";s:30:"class.tx_ncstaticfilecache.php";s:4:"168b";s:21:"ext_conf_template.txt";s:4:"0cf8";s:12:"ext_icon.gif";s:4:"03df";s:17:"ext_localconf.php";s:4:"372b";s:14:"ext_tables.php";s:4:"6285";s:14:"ext_tables.sql";s:4:"1a0f";s:16:"locallang_db.xml";s:4:"441a";s:15:"cli/cleaner.php";s:4:"7ed9";s:23:"cli/pre_4.1/cleaner.php";s:4:"ed3e";s:20:"cli/pre_4.1/conf.php";s:4:"2d36";s:14:"doc/_.htaccess";s:4:"63d1";s:14:"doc/manual.sxw";s:4:"4e0b";s:48:"modfunc1/class.tx_ncstaticfilecache_modfunc1.php";s:4:"269e";s:22:"modfunc1/locallang.php";s:4:"6a50";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"Changelog";s:4:"808f";s:36:"class.tx_ncstaticfilecache.debug.php";s:4:"225e";s:30:"class.tx_ncstaticfilecache.php";s:4:"1262";s:21:"ext_conf_template.txt";s:4:"0cf8";s:12:"ext_icon.gif";s:4:"03df";s:17:"ext_localconf.php";s:4:"372b";s:14:"ext_tables.php";s:4:"378e";s:14:"ext_tables.sql";s:4:"1a0f";s:16:"locallang_db.xml";s:4:"0cb0";s:15:"cli/cleaner.php";s:4:"7ed9";s:23:"cli/pre_4.1/cleaner.php";s:4:"ed3e";s:20:"cli/pre_4.1/conf.php";s:4:"2d36";s:14:"doc/_.htaccess";s:4:"63d1";s:14:"doc/manual.sxw";s:4:"6a25";s:50:"infomodule/class.tx_ncstaticfilecache_modfunc1.php";s:4:"c6ae";s:24:"infomodule/locallang.php";s:4:"022c";}',
 	'suggests' => array(
 	),
 );
