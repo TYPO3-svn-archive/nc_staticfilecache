b93bb591a87413a8583e5e29c6ea725776146c0d
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-22 09:10:35 +0000

    * Fixed bug #6158: Scheduler tasks: missing ext_autoload (Thanks to Peter Schuster)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nc_staticfilecache/trunk@29202 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_autoload.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'tx_ncstaticfilecache_tasks_processdirtypages' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages.php'),
+	'tx_ncstaticfilecache_tasks_processdirtypages_additionalfieldprovider' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages_AdditionalFieldProvider.php'),
+	'tx_ncstaticfilecache_tasks_removeexpiredpages' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages.php'),
+	'tx_ncstaticfilecache_tasks_removeexpiredpages_additionalfieldprovider' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages_AdditionalFieldProvider.php'),
+);
+?>
