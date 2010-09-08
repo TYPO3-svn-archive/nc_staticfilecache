6c3259aa269c2d60263d15fdedb1c0413b16921b
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-08 13:34:15 +0000

    Raised version to 2.3.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nc_staticfilecache/trunk@37823 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "nc_staticfilecache"
+# Extension Manager/Repository config file for ext "nc_staticfilecache".
 #
-# Auto generated 23-02-2008 17:11
+# Auto generated 08-09-2010 15:32
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Transparent static file cache solution using mod_rewrite and mod_expires. Increase response times for static pages by a factor of 230!',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.3.1',
+	'version' => '2.3.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,14 +34,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.2.11-',
+			'typo3' => '4.2.11-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"Changelog";s:4:"808f";s:36:"class.tx_ncstaticfilecache.debug.php";s:4:"225e";s:30:"class.tx_ncstaticfilecache.php";s:4:"1262";s:21:"ext_conf_template.txt";s:4:"0cf8";s:12:"ext_icon.gif";s:4:"03df";s:17:"ext_localconf.php";s:4:"372b";s:14:"ext_tables.php";s:4:"378e";s:14:"ext_tables.sql";s:4:"1a0f";s:16:"locallang_db.xml";s:4:"0cb0";s:15:"cli/cleaner.php";s:4:"7ed9";s:23:"cli/pre_4.1/cleaner.php";s:4:"ed3e";s:20:"cli/pre_4.1/conf.php";s:4:"2d36";s:14:"doc/_.htaccess";s:4:"63d1";s:14:"doc/manual.sxw";s:4:"6a25";s:50:"infomodule/class.tx_ncstaticfilecache_modfunc1.php";s:4:"c6ae";s:24:"infomodule/locallang.php";s:4:"022c";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"Changelog";s:4:"962e";s:10:"README.txt";s:4:"2972";s:30:"class.tx_ncstaticfilecache.php";s:4:"b52f";s:42:"class.tx_ncstaticfilecache_crawlerhook.php";s:4:"e98e";s:16:"ext_autoload.php";s:4:"eb7f";s:21:"ext_conf_template.txt";s:4:"9a63";s:12:"ext_icon.gif";s:4:"03df";s:17:"ext_localconf.php";s:4:"c470";s:14:"ext_tables.php";s:4:"378e";s:14:"ext_tables.sql";s:4:"8387";s:16:"locallang_db.xml";s:4:"3560";s:15:"cli/cleaner.php";s:4:"45e1";s:25:"doc/gzip.realurl.htaccess";s:4:"1fe7";s:32:"doc/gzip.simulateStatic.htaccess";s:4:"dd32";s:14:"doc/manual.sxw";s:4:"7ec3";s:26:"doc/plain.realurl.htaccess";s:4:"9617";s:33:"doc/plain.simulateStatic.htaccess";s:4:"7a3d";s:52:"infomodule/class.tx_ncstaticfilecache_infomodule.php";s:4:"b8dc";s:24:"infomodule/locallang.php";s:4:"022c";s:60:"tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages.php";s:4:"1917";s:84:"tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages_AdditionalFieldProvider.php";s:4:"387a";s:61:"tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages.php";s:4:"5f45";s:85:"tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages_AdditionalFieldProvider.php";s:4:"f8f9";}',
 	'suggests' => array(
 	),
 );
