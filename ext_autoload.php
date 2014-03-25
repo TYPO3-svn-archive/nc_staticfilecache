7d4aa6fe7e58a9ec70093210ca13633cf47c92d2
spyker <spyker@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-25 18:08:05 +0000

    cleanup ext_autoload.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nc_staticfilecache/trunk@84491 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,8 +1,11 @@
 <?php
+$extensionPath = t3lib_extMgm::extPath('nc_staticfilecache');
 return array(
-	'tx_ncstaticfilecache_tasks_processdirtypages' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages.php'),
-	'tx_ncstaticfilecache_tasks_processdirtypages_additionalfieldprovider' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages_AdditionalFieldProvider.php'),
-	'tx_ncstaticfilecache_tasks_removeexpiredpages' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages.php'),
-	'tx_ncstaticfilecache_tasks_removeexpiredpages_additionalfieldprovider' => t3lib_extMgm::extPath('nc_staticfilecache', 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages_AdditionalFieldProvider.php'),
+	'tx_ncstaticfilecache_tasks_processdirtypages'                          => $extensionPath . 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages.php',
+	'tx_ncstaticfilecache_tasks_processdirtypages_additionalfieldprovider'  => $extensionPath . 'tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages_AdditionalFieldProvider.php',
+	'tx_ncstaticfilecache_tasks_removeexpiredpages'                         => $extensionPath . 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages.php',
+	'tx_ncstaticfilecache_tasks_removeexpiredpages_additionalfieldprovider' => $extensionPath . 'tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages_AdditionalFieldProvider.php',
+	'tx_ncstaticfilecache_infomodule'                                       => $extensionPath . 'infomodule/class.tx_ncstaticfilecache_infomodule.php',
+	'tx_ncstaticfilecache'                                                  => $extensionPath . 'class.tx_ncstaticfilecache.php',
+	'tx_ncstaticfilecache_crawlerhook'                                      => $extensionPath . 'class.tx_ncstaticfilecache_crawlerhook.php',
 );
-?>
