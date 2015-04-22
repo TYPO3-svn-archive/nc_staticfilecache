b559b1153a56f89f2e49402aafe6ed1caf1e3e55
spyker <spyker@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-22 13:06:42 +0000

    Format localconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nc_staticfilecache/trunk@88882 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,11 +10,13 @@ if (!defined('TYPO3_MODE')) {
 
 // Register with "crawler" extension:
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['procInstructions']['tx_ncstaticfilecache_clearstaticfile'] = 'clear static cache file';
+
 // Hook to process clearing static cached files if "crawler" extension is active:
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['headerNoCache'][$_EXTKEY] = 'SFC\\NcStaticfilecache\\Hook\\Crawler->clearStaticFile';
 
 // Create cache
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['insertPageIncache'][$_EXTKEY] = 'SFC\\NcStaticfilecache\\StaticFileCache';
+
 // Catch Ctrl + Shift + reload (only works when backend user is logged in)
 //	You need the be_typo_user cookie detection enabled in the rewrite rules
 //	for this to work.
